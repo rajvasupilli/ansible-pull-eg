c2e2c347c151a284b710877146c7327cc846a5b5
rajvasupilli <30533295+rajvasupilli@users.noreply.github.com>
2021-02-14 19:16:52 +0530

    Update install_ansible.sh

--- install_ansible.sh
+++ install_ansible.sh
@@ -4,4 +4,4 @@ sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install -y ansible
 #`ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y`
-sudo ansible-pull -U https://github.com/rajvasupilli/ansible-pull-eg.git -d ansible-code/
+sudo ansible-pull -U https://github.com/rajvasupilli/ansible-pull-eg.git
